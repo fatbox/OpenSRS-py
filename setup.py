0b1965e9dbfddefa8da50b56f7f20112624e0f4a
Evan Borgstrom <evan@fatbox.ca>
2012-01-03 11:40:47 -0500

    Repack for cheeseshop

--- setup.py
+++ setup.py
@@ -2,8 +2,22 @@
 
 from setuptools import setup
 
-setup(name="OpenSRS",
-      version='0.1',
-      packages=['opensrs', ],
-      install_requires=['httplib2', ],
-     )
+setup(
+        name="OpenSRS",
+        packages=['opensrs', ],
+        install_requires=['httplib2', ],
+        version='0.1.1',
+        description='Higher level Python interface to the OpenSRS XML API',
+        long_description=open('README.md').read(),
+        author='Evan Borgstrom',
+        author_email='evan@fatbox.ca',
+        url='https://github.com/fatbox/OpenSRS-py',
+        classifiers=[
+            'Development Status :: 4 - Beta',
+            'Intended Audience :: Developers',
+            'License :: OSI Approved :: BSD License',
+            'Natural Language :: English',
+            'Topic :: Software Development :: Libraries',
+            'Topic :: Software Development :: Libraries :: Python Modules',
+            ],
+        )
